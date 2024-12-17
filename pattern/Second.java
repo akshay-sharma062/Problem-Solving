121872c266d2fd97780b6dc6e57805963557c82a
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2024-12-17 16:53:44 +0530

    2nd pattern

---
+++ pattern/Second.java
@@ -0,0 +1,23 @@
+/*
+ A
+ BB
+ CCC
+ DDDD
+ EEEEE
+ */
+
+
+
+
+class Second{
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 0; i <= n; i++) {
+           
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char)(65+i) );
+            }
+            System.out.println();
+        }
+    }
+}
