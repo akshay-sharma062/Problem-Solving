d27d280f27b60a795a2832fe8c42b51a5fa14114
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2024-12-25 00:11:28 +0530

    13th pattern

--- pattern/Thirteenth.java
+++ pattern/Thirteenth.java
@@ -1,5 +1,44 @@
+//  * * * * *
+//   * * * *
+//    * * *
+//     * *
+//      *
+//      *
+//     * *
+//    * * *   
+//   * * * *
+//  * * * * * 
+
 public class Thirteenth {
     public static void main(String[] args) {
         int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+                
+            }
+            for (int j = n; j >i ; j--) {
+
+                System.out.print("* ");
+                
+            }  
+            if (i<n-1) {
+                
+                System.out.println();  
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i+1; j--) {
+                System.out.print(" ");
+                
+            }
+            for (int j = 0; j <i+1 ; j++) {
+
+                System.out.print("* ");
+                
+            }  
+            System.out.println();          
+        }
     }
 }
