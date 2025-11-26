157c1be3541773da158945df39f4c0be922b8788
Akshay Sharma <akshay.ku.sharma.062@gmail.com>
2025-11-26 23:22:35 +0530

    Implement function to find max and min in array

---
+++ MaxAndMin.js
@@ -0,0 +1,16 @@
+const arr = [10, 5, 30, 2, 50];
+
+let max = arr[0];
+let min = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+}
+
+console.log("Greatest:", max);
+console.log("Smallest:", min);
